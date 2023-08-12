ea9ba7e26b5d5de5ec1d8d6265396d82a6af10d8
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2023-08-13 02:51:55 +0500

    Added toggle for turrets.

--- Assets/Assets/Scripts/GroundPlacementController.cs
+++ Assets/Assets/Scripts/GroundPlacementController.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GroundPlacementController : MonoBehaviour
 {
@@ -10,6 +13,8 @@ public class GroundPlacementController : MonoBehaviour
 
     private float mouseWheelRotation;
     private int currentPrefabIndex = -1;
+    [SerializeField]
+    private ToggleGroup tg;
 
     private bool started;
     GameModeSwitcher switcher;
@@ -30,47 +35,92 @@ public class GroundPlacementController : MonoBehaviour
     private void Update()
     {
         HandleNewObjectHotkey();
-
         if (currentPlaceableObject != null)
         {
             MoveCurrentObjectToMouse();
             RotateFromMouseWheel();
             ReleaseIfClicked();
+            
         }
     }
 
     private void HandleNewObjectHotkey()
     {
-        for (int i = 0; i < placeableObjectPrefabs.Length; i++)
+
+
+        Toggle selected = tg.GetFirstActiveToggle();
+
+        int current_turret = -1;
+
+        if (selected != null)
         {
-            if (Input.GetKeyDown(KeyCode.Alpha0 + 1 + i))
+            switch (selected.name)
             {
-                if (PressedKeyOfCurrentPrefab(i))
-                {
-                    Destroy(currentPlaceableObject);
-                    currentPrefabIndex = -1;
-                }
-                else
-                {
-                    if (currentPlaceableObject != null)
-                    {
-                        Destroy(currentPlaceableObject);
-                    }
-
-                    currentPlaceableObject = Instantiate(placeableObjectPrefabs[i]);
-                    currentPrefabIndex = i;
-                }
-
-                break;
+                case ("Ballista"):
+                    current_turret = 0;
+                    break;
+                case ("Garage"):
+                    current_turret = 1;
+                    break;
             }
         }
+
+        if(current_turret != -1)
+        {
+            if (PressedKeyOfCurrentPrefab(current_turret))
+            {
+                Destroy(currentPlaceableObject);
+                currentPrefabIndex = -1;
+            }
+            else
+            {
+                if(currentPlaceableObject != null)
+                {
+                    Destroy(currentPlaceableObject);
+                }
+                currentPlaceableObject = Instantiate(placeableObjectPrefabs[current_turret]);
+                currentPrefabIndex = current_turret;
+            }
+        }
+
+
+        //for (int i = 0; i < placeableObjectPrefabs.Length; i++)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Alpha0 + 1 + i))
+        //    {
+        //        if (PressedKeyOfCurrentPrefab(i))
+        //        {
+        //            Destroy(currentPlaceableObject);
+        //            currentPrefabIndex = -1;
+        //        }
+        //        else
+        //        {
+        //            if (currentPlaceableObject != null)
+        //            {
+        //                Destroy(currentPlaceableObject);
+        //            }
+
+        //            currentPlaceableObject = Instantiate(placeableObjectPrefabs[i]);
+        //            currentPrefabIndex = i;
+        //        }
+
+        //        break;
+        //    }
+        //}
     }
 
     private bool PressedKeyOfCurrentPrefab(int i)
     {
         return currentPlaceableObject != null && currentPrefabIndex == i;
     }
-
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
     private void MoveCurrentObjectToMouse()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -88,14 +138,14 @@ public class GroundPlacementController : MonoBehaviour
 
     private void RotateFromMouseWheel()
     {
-        Debug.Log(Input.mouseScrollDelta);
+       
         mouseWheelRotation += Input.mouseScrollDelta.y;
         currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
     }
 
     private void ReleaseIfClicked()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)&& !IsPointerOverUIObject())
         {
             currentPlaceableObject = null;
         }
